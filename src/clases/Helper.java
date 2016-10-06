5db1a69c50a17a97ac92f63edbaeaa345d54e5d7
Gabriel <gabrielomar2809@gmail.com>
2016-10-06 17:22:01 -0500

    Interfaz Creada

---
+++ src/clases/Helper.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author gcoronad2
+ */
+public class Helper {
+
+    public static void mensaje(Component ventana, String mensaje, int tipo_mensaje) {
+        switch (tipo_mensaje) {
+            case 1:
+                JOptionPane.showMessageDialog(ventana, mensaje, "INFO", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(ventana, mensaje, "ADVERTENCIA", JOptionPane.WARNING_MESSAGE);
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(ventana, mensaje, "ERROR", JOptionPane.ERROR_MESSAGE);
+                break;
+        }
+    }
+
+    public static void limpiadoTabla(JTable tabla1) {
+        int nf, nc;
+        nf = tabla1.getRowCount();
+        nc = tabla1.getColumnCount();
+        for (int i = 0; i < nf; i++) {
+            for (int j = 0; j < nc; j++) {
+                tabla1.setValueAt("", i, j);
+            }
+        }
+    }
+
+    public static void porDefectoTabla(JTable tabla1) {
+        DefaultTableModel tm;
+        tm = (DefaultTableModel) tabla1.getModel();
+        tm.setRowCount(0);
+        tm.setColumnCount(0);
+    }
+
+    public static void habilitarBotones(JButton[] botones) {
+        for (int i = 0; i < botones.length; i++) {
+            botones[i].setEnabled(true);
+        }
+    }
+
+    public static void deshabilitarBotones(JButton[] botones) {
+        for (int i = 0; i < botones.length; i++) {
+            botones[i].setEnabled(false);
+        }
+    }
+
+    public static int[][] pasoDeDatos(JTable tabla1){
+        int nf, nc;
+        int m[][];
+        nf = tabla1.getRowCount();
+        nc = tabla1.getColumnCount();
+        m = new int [nf][nc];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                m[i][j] = (int)tabla1.getValueAt(i, j);
+            }
+        }
+        return m;
+    }
+
+}
